e2c86c140ca507b487ea6a23c661a823b7dfca7f
natam.chmura <natam.chmura@selectinvestimentos.com>
2023-02-08 13:44:54 -0300

    feat: Header, Screen and Footer component

---
+++ src/components/Footer.styles.ts
@@ -0,0 +1,18 @@
+import { styled } from "../styles";
+import { Heart } from "phosphor-react";
+export const FooterContainer = styled("footer", {
+    width: "100%",
+    height: "3.5rem",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+
+    background: "$gray800"
+});
+
+export const FooterHeart = styled(Heart, {
+    margin: "0 0.35rem",
+    color: "$gray100",
+
+    
+});
---
+++ src/components/Header.styles.ts
@@ -0,0 +1,24 @@
+import { styled } from "../styles";
+
+export const HeaderContainer = styled("header", {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "3.5rem",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    background: "$gray800"
+});
+
+export const HeaderIcons = styled("div", {
+    margin: "0 auto"
+});
+
+export const HeaderIcon = styled("a", {
+    margin: "0 1rem",
+    textDecoration: "none"
+});
+
