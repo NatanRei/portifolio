13d5f53e62f59c171232b0de38676849296870c1
natam.chmura <natam.chmura@selectinvestimentos.com>
2023-02-08 14:47:29 -0300

    feat: change div to section

--- src/components/Screen.styles.ts
+++ src/components/Screen.styles.ts
@@ -1,6 +1,6 @@
 import { styled } from "../styles";
 
-export const ScreenContainer = styled("div", {
+export const ScreenContainer = styled("section", {
     width: "100%",
     minHeight: "100vh",
     display: "flex",
