b5591e8b325a77cf98fd7b988d6bbc1949e31e8d
NatanRei <nreischmura@gmail.com>
2023-02-11 17:17:21 -0300

    feat: um pouco de gambiarra se faz sempre essencial para garantir a satisifação dos desejos mirabolantes do cliente

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
-import { Screen } from "../components/Screen";
+import { ParallaxAnimation } from "../components/ParalaxAnimation";
 import { HomeContainer } from "./Home.styles";
 import { LandingPage } from "./LandingPage/LandingPage";
 
@@ -9,8 +9,10 @@ export function Home() {
     return (
         <HomeContainer>
             <Header />
-            <LandingPage/>
-            <Footer />
-        </HomeContainer>
+            <ParallaxAnimation />
+
+            {/* <LandingPage/> */}
+            {/* <Footer /> */}
+         </HomeContainer>
     );
 }
--- src/pages/LandingPage/components/Initial.tsx
+++ src/pages/LandingPage/components/Initial.tsx
@@ -1,4 +1,5 @@
-import { TextLoop } from "../../../components/TextLoop";
+
+import { ParallaxAnimation } from "../../../components/ParalaxAnimation";
 import { InitialContainer, SubTitle, Title, TitleContainer } from "./Initial.styles";
 
 export function Initial() {
@@ -8,11 +9,7 @@ export function Initial() {
             <Title>NATAN REIS CHMURA</Title>
             <SubTitle>Full Stack Developer</SubTitle>
             </TitleContainer>
-            <TextLoop />
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                <path fill="#797979" fillOpacity="0.5" d="M0,64L30,69.3C60,75,120,85,180,80C240,75,300,53,360,80C420,107,480,181,540,181.3C600,181,660,107,720,112C780,117,840,203,900,245.3C960,288,1020,288,1080,277.3C1140,267,1200,245,1260,245.3C1320,245,1380,267,1410,277.3L1440,288L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z">
-                </path>
-            </svg>
+            <ParallaxAnimation />
         </InitialContainer>
     );
 }
