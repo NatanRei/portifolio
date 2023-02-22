0f99c8e134ef3f42f9f9c824a940b43140d3034f
NatanRei <nreischmura@gmail.com>
2023-02-22 18:13:43 -0300

    I'm felling the growth

---
+++ src/pages/LandingPage/components/About.styles.ts
@@ -0,0 +1,26 @@
+import { styled } from "../../../styles";
+
+export const AboutContainer = styled("div", {
+    width: "100%",
+    margin: "1rem 7rem",
+    'h2': {
+        fontSize: '$2xl'
+    }
+
+});
+
+export const BoxContainer = styled("div", {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignContent: "center"
+});
+
+export const ParagraphContainer = styled("div", {
+    fontSize: "$lg",
+    width: "27%",
+    margin: "3rem 7rem",
+    height: "auto"
+});
+
+
--- src/pages/LandingPage/components/About.tsx
+++ src/pages/LandingPage/components/About.tsx
@@ -1,5 +1,33 @@
+import { AboutContainer, BoxContainer, ParagraphContainer } from "./About.styles";
+import {format, formatDistanceToNow} from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR';
+
 export function About() {
+    const initOfNatanProgramingCarrer = new Date("01/09/2020");
+
+    const publishedDateRelativeToNow = formatDistanceToNow(initOfNatanProgramingCarrer, {
+        locale: ptBR,
+        addSuffix: true,
+    });
+
+
+    console.log(publishedDateRelativeToNow);
+
     return (
-        <h1>About</h1>
+        <AboutContainer>
+            <h2>Olá,</h2>
+            <BoxContainer>
+            <ParagraphContainer>
+                Sou desenvolvedor de softwares, aplicações web e afins.
+                Desde muito tempo, venho mantendo contato com o universo da tecnologia,
+                mas iniciei os trabalhos com programação Web em meados de setembro ou outubro de 2020, {publishedDateRelativeToNow}
+            </ParagraphContainer>
+            {/* colocar um contador aqui */}
+            </BoxContainer>
+            <ParagraphContainer>
+                Desde lá, sempre busco evolução constante nas áreas que a programação e tecnologia abrangem (tudo?), mantendo-me alinhado com o mercado.
+            </ParagraphContainer>
+
+        </AboutContainer>
     );
 }
