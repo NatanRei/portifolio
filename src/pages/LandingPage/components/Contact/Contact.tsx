e0839744d46a3e4890463ea0144f499f1f07eced
NatanRei <nreischmura@gmail.com>
2023-06-21 19:53:42 -0300

    feat: brincando com as animações

--- src/pages/LandingPage/components/Contact/Contact.tsx
+++ src/pages/LandingPage/components/Contact/Contact.tsx
@@ -11,11 +11,10 @@ export function Contact() {
     const element: any = elementRef.current
     let translate = 0;
         if(element) {
-            const scrollNow = window.pageYOffset
-            const elementLenght = element.offsetHeight
+            const elementLength = element.offsetHeight
             const initOfElement = element.offsetTop - (element.offsetHeight)
-            const zero = (scrollNow - initOfElement) < 0 ? 0 : (scrollNow - initOfElement)
-            let percentage = (zero * 100) / (elementLenght * 1)
+            const zero = (offsetY - initOfElement) < 0 ? 0 : (offsetY - initOfElement)
+            let percentage = (zero * 100) / (elementLength * 1)
             percentage = percentage > 100 ? 100 : percentage < 0 ? 0 : percentage
             translate = 33.3 * ( 1- (percentage / 100))
         }
