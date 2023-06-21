3ee4f1fa85430e8426aae61f0e2de6d05e05cacc
natam.chmura <natam.chmura@selectinvestimentos.com>
2023-06-21 11:18:29 -0300

    style: changing border to shadow

--- src/pages/Home.styles.ts
+++ src/pages/Home.styles.ts
@@ -6,13 +6,20 @@ export const HomeContainer = styled("main", {
 });
 
 export const Paragraph = styled("p", {
-    border: "1px solid $gray700",
+    backgroundColor: "$gray950",
     padding: "2rem",
     borderRadius: 8,
     fontSize: "$lg",
     width: "27%",
     margin: "3rem 7rem",
     height: "auto",
+
+    $$shadowGrayTop: '$colors$black',
+    $$shadowGrayBottom: '$colors$gray1000',
+
+    boxShadow: ".4rem .4rem .7rem $$shadowGrayTop, -.25rem -.2rem .6rem $$shadowGrayBottom",
+
+    
     "a": {
         textDecoration: "none",
         color: 'white',
